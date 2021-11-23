543afea5f48e1fc7c70fb67021e750960cfb5bac
Su Ping (uidp3524) <uidp3524@hzh1140u.v01.net>
2021-11-23 16:28:17 +0800

    optimeze

--- main.cpp
+++ main.cpp
@@ -15,52 +15,65 @@
 #include "armpmu_lib.h"
 
 int enable_access_qnx_register();
-int main()
-{
-	double seconds , ipc_value, cycle2clock;
-    uint64_t cpu_cycles, inst_retired, evt_value[6];
-	int 	nmax = 1024,i,n; 
 
-	struct timeval start,end;
+int start_pmu(){
 
     enable_access_qnx_register();
-    //dump_pmu_reg();
     enableUser();
+	enableCounter();
+	enablePMU();
+	enableEvtCounter();
+	setEvtCount();	
+	isb();
+}
 
-    while(1){
-	    enableCounter();
-	    enablePMU();
-	    enableEvtCounter();
-	    setEvtCount();	
-	    isb();
+void print_pmu()
+{
+        double ipc_value;
+        uint64_t cpu_cycles, evt_value[6];
+        int n;
 
-        gettimeofday(&start,NULL);
-        sleep(1);
-	    gettimeofday(&end,NULL);
-	
         cpu_cycles = getPMUCount();
 	    for(n = 0; n<6; n++){
 		    evt_value[n] = getEvtCount(n);
 	    }
-	    seconds = (end.tv_sec - start.tv_sec)+1.0e-6 * (end.tv_usec - start.tv_usec);
-	    cycle2clock = (double)cpu_cycles/1000000000/seconds ;
 	    ipc_value = (double)evt_value[0]/(double)cpu_cycles ;
 	    printf("\n=======\ncpu cycles: %ld\n",  cpu_cycles);
-        /* for(n = 0; n<6; n++){ */
-		    /* printf("evt%d:%lld\n", n, evt_value[n]); */
-        /* } */
+
+		printf("inst_retired:      %lld\n", evt_value[0]);
+		printf("l1d cache:         %lld\n",  evt_value[1]);
+		printf("l1d cache refill:  %lld\n",  evt_value[2]);
+		printf("l2d cache:         %lld\n",  evt_value[3]);
+		printf("l2d cache refill:  %lld\n", evt_value[4]);
+		printf("BR miss:           %lld\n", evt_value[5]);
+
 	    printf("L1_MISS rate:%.3f \n", (double)evt_value[2]/(double)(evt_value[1]));
 	    printf("L2_MISS rate:%.3f \n", (double)evt_value[4]/(double)(evt_value[3]));
 	    printf("Mispredicted branch:%ld \n", evt_value[5]);
-	    printf("%.3f s %.3f Ghz ipc = %.3f\n", seconds, cycle2clock, ipc_value);
-    
-	    stopCounter();
-	    disableEvtCounter();
-	    disablePMU();
-	    isb();
-	    //dump_pmu_reg();
-    }
+	    printf("ipc = %.3f\n", ipc_value);
+}
 
+int stop_pmu(){
+	stopCounter();
+	disableEvtCounter();
+	disablePMU();
+	isb();
+}
+
+int main()
+{
+	double seconds; 
+	struct timeval start,end;
+    while(1){
+        start_pmu();
+        gettimeofday(&start,NULL);
+        sleep(1);
+	    gettimeofday(&end,NULL);
+        print_pmu();
+        stop_pmu();
+	    seconds = (end.tv_sec - start.tv_sec)+1.0e-6 * (end.tv_usec - start.tv_usec);
+	    printf("cost:%.3f s\n", seconds);
+    }
     return 0;
 }
 
